99a7f8af60c180c031afeebfe057ad56b4f06d40
carlosmast23 <carlosmast2301@hotmail.es>
2021-10-19 11:52:01 -0500

    Agregada Modificaciones al documento de la Tesis

--- workspace/application/config/config.php
+++ workspace/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/molinuco/workspace/';
+$config['base_url'] = 'http://192.168.100.4/molinuco/workspace/';
 
 /*
 |--------------------------------------------------------------------------
